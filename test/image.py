20dc72b51c2726d21d4b55dd60aa9c3e526c525b
juliennoe <juliennoe.job@gmail.com>
2020-08-11 16:06:06 +0200

    openImage

--- test/image.py
+++ test/image.py
@@ -1,6 +1,28 @@
 import requests
 import json
-from PIL import Image
+from PIL import Image, ImageTk
 import os
+from tkinter import Tk, Button, Canvas, PhotoImage
 
-print('hello world')
+imageSoleil = 'GITHUB/openImagePython/test/SOLEIL.png'
+
+fenetre = Tk()
+fenetre.attributes('-fullscreen', True)
+
+w, h = fenetre.winfo_screenwidth(),fenetre.winfo_screenheight()
+
+image = Image.open(imageSoleil)
+image = image.resize((w, h))
+
+photo = ImageTk.PhotoImage(image)
+
+can = Canvas(fenetre, highlightthickness=0)
+can.create_image(0, 0, anchor='nw', image=photo)
+can.pack(fill='both', expand=1)
+
+Button(can,text='Quitter', command=fenetre.destroy).pack()
+
+fenetre.mainloop()
+#print('hello world')
+#image = Image.open('GITHUB/openImagePython/test/SOLEIL.png')
+
